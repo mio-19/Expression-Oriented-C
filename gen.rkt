294cd4e50138633acac728d008a65abb2e109ce4
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-29 15:04:11 +0000

    up

--- gen.rkt
+++ gen.rkt
@@ -18,6 +18,8 @@
     [(_ prefix (f (arg : type) ...) -> result body) (LINE prefix" "result" "f"("(LIST (++ type" "arg) ...)"){"body"}")]
     [(_ (f (arg : type) ...) -> result body) (LINE result" "f"("(LIST (++ type" "arg) ...)"){"body"}")]}}
 
+{define internal-prefix "___EOC___internal___WAHAHA___"}
+
 {define temp1 "___EOC___temp___VARIABLE___1___"}
 {define local-inline "static inline"}
 
@@ -31,6 +33,10 @@
    (DEFINE ("the" "type" "value") (++ "({(type) "temp1"=(value);"temp1"})"))
    (DEFINE ("as" "type" "value") "(type)(value)")
    (DEFINE-FUNC local-inline ("mkvoid") -> Void "")
+   
+   (DEFINE ("when" "b") (++ "((b)?"internal-prefix"when_helper"))
+   (DEFINE ((++ internal-prefix"when_helper") "body") (++ "({body}):"internal-prefix"when_helper_2"))
+   (DEFINE ((++ internal-prefix"when_helper_2") "body") "({body}))")
    )}
 
 (display-to-file prelude "prelude.h" #:exists 'replace)
--- prelude.h
+++ prelude.h
@@ -4,3 +4,6 @@
 #define the(type,value) ({(type) ___EOC___temp___VARIABLE___1___=(value);___EOC___temp___VARIABLE___1___})
 #define as(type,value) (type)(value)
 static inline void mkvoid(void){}
+#define when(b) ((b)?___EOC___internal___WAHAHA___when_helper
+#define ___EOC___internal___WAHAHA___when_helper(body) ({body}):___EOC___internal___WAHAHA___when_helper_2
+#define ___EOC___internal___WAHAHA___when_helper_2(body) ({body}))
