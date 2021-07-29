c07071caa4f905dd3e2f93cebea8607b17b2a867
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-29 14:52:32 +0000

    up

--- prelude.h
+++ prelude.h
@@ -1,5 +1,6 @@
 #define let auto
 #define begin(body) ({body})
 #define start(body) {return ({body});}
-#define the(type,value) ({type ___EOC___temp___VARIABLE___1___=value;___EOC___temp___VARIABLE___1___})
+#define the(type,value) ({(type) ___EOC___temp___VARIABLE___1___=(value);___EOC___temp___VARIABLE___1___})
+#define as(type,value) (type)(value)
 static inline void mkvoid(void){}
