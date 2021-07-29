7937fe7dfeb6ec4f5be7e13fd3fc4c5a0258adfd
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-29 14:34:33 +0000

    up

--- gen.rkt
+++ gen.rkt
@@ -11,9 +11,13 @@
 {define ... "..."}
 {define VA_ARGS "__VA_ARGS__"}
 
+{define temp1 "___EOC___temp___VARIABLE___1___"}
+
 {define prelude
   (BEGIN
-   (DEFINE ("begin" "body") (++ "({" "body" "})"))
-   (DEFINE ("start" "body") (++ "{return ({" "body" "});}")))}
+   (DEFINE ("begin" "body") "({body})")
+   (DEFINE ("start" "body") "{return ({body});}")
+   (DEFINE ("the" "type" "value") (++ "({type "temp1"=value;"temp1"})"))
+   )}
 
 (display-to-file prelude "prelude.h" #:exists 'replace)
