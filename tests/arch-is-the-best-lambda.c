f04108c90b2d9aaaec279581ca318fa25a650d3f
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-30 02:46:55 +0000

    up

--- tests/arch-is-the-best-lambda.c
+++ tests/arch-is-the-best-lambda.c
@@ -3,5 +3,7 @@
 #include <prelude.h>
 int main(void) start(
    let arch_best = lambda(void, (void))(puts("Arch is the best!"); mkvoid());
+   void (*arch_best2)(void) = lambda(void, (void))(puts("Arch is the best!"); mkvoid());
    arch_best();
+   arch_best2();
    EXIT_SUCCESS)
