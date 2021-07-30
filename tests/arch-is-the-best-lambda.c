663854f5a689c79e2a46341b5f244e5d4f7555c1
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-30 02:45:54 +0000

    up

--- tests/arch-is-the-best-lambda.c
+++ tests/arch-is-the-best-lambda.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 #include <prelude.h>
 int main(void) start(
-   void (*arch_best)(void) = lambda(void, (void))(puts("Arch is the best!"); mkvoid());
+   let arch_best = lambda(void, (void))(puts("Arch is the best!"); mkvoid());
    arch_best();
    EXIT_SUCCESS)
