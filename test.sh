a9e183a03cb636dfe176c26e9743f5a8d0346ef5
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-07-30 02:32:28 +0000

    up

--- test.sh
+++ test.sh
@@ -9,14 +9,17 @@ for test in *.c; do
   gcc -I.. "$test"
   ./a.out > check-stdout
   rm a.out
-  if [ "$(cat check-stdout)" != "$(cat "testname.stdout")" ]; then
+  if [ "$(cat check-stdout)" != "$(cat "$testname.stdout")" ]; then
     echo "Failed: $testname"
     echo "Expected:"
-    cat "testname.stdout"
+    cat "$testname.stdout"
     echo "Got:"
     cat check-stdout
+    rm check-stdout
     exit 1
   fi
 done
+
+rm -f check-stdout
 
 echo "Done!"
